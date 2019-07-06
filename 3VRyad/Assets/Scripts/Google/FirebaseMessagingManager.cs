677311733910feeacc29d05c1903e99f0fa76c58
VIDO <vido1@yandex.ru>
2019-07-06 19:35:30 +0400

    добавление файла месенджера

---
+++ 3VRyad/Assets/Scripts/Google/FirebaseMessagingManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseMessagingManager : MonoBehaviour
+{
+    public static FirebaseMessagingManager Instance; // Синглтон
+
+    public void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this.gameObject); //Delete duplicate
+            return;
+        }
+        else
+        {
+            Instance = this; //Make this object the only instance            
+        }
+        if (Application.isPlaying)
+        {
+            DontDestroyOnLoadManager.DontDestroyOnLoad(gameObject); //Set as do not destroy            
+        }
+    }
+
+    //public void Start()
+    //{
+    //    Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+    //    Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    //}
+
+    //public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    //{
+    //    UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    //}
+
+    //public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    //{
+    //    UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    //}
+}
